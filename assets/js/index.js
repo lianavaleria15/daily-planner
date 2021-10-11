6f599dc780c861a31821af9f21cfd9378d1fa5a2
liana valeria <liana.valeria15@gmail.com>
2021-10-11 15:05:54 +0100

    used map function to construct time blocks and appended them to container

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,3 +6,30 @@ const today = moment().format("Do MMMM YYYY");
 
 //append current day to date container
 dateContainer.text(today);
+
+//get business hours and store them into an array
+const businessHours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+
+//build time blocks
+const constructTimeBlocks = (businessHours) => {
+  const timeBlock = (element, index) => {
+    const timeBlockDiv = `<div class="row">
+    <div class="col time-item">${element}</div>
+    <div class="col-8 activity-item">Activity here</div>
+    <div class="col save-item"><i class="fas fa-save"></i></div>
+  </div>`;
+    return timeBlockDiv;
+  };
+  return businessHours.map(timeBlock).join("");
+};
+
+// render time blocks
+const renderTimeBlocks = () => {
+  const timeBlocks = constructTimeBlocks(businessHours);
+  $(".container").append(timeBlocks);
+};
+
+//on window load
+window.onload = () => {
+  renderTimeBlocks();
+};
