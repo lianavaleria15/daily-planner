21b5bedb2a575204cf4505c1916762df6e436d91
liana valeria <liana.valeria15@gmail.com>
2021-10-11 13:59:56 +0100

    used moment js to get current day and append it to date container

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,8 @@
-console.log("hello");
+//target date container
+const dateContainer = $("#currentDay");
+
+//get current day and format it
+const today = moment().format("Do MMMM YYYY");
+
+//append current day to date container
+dateContainer.text(today);
