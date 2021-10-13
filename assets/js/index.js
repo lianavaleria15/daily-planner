d48272177b282b6c90fed6dc18030d56725ca81a
liana valeria <liana.valeria15@gmail.com>
2021-10-13 21:01:16 +0100

    added recent code

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,47 +12,50 @@ const businessHours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
 //build time blocks
 const constructTimeBlocks = (businessHours) => {
-  const timeBlock = (element, index) => {
+  const timeBlock = (element) => {
     const timeBlockDiv = `<div class="row">
     <div class="col time-item">${element}</div>
-    <div class="col-8 activity-item">Activity here</div>
+    <div class="col-8 past"><textarea class="textarea"></textarea></div>
     <div  class="col save-item"><i class="fas fa-save"></i></div>
   </div>`;
-
     return timeBlockDiv;
   };
+
   return businessHours.map(timeBlock).join("");
 };
 
 // render time blocks
 const renderTimeBlocks = () => {
   const timeBlocks = constructTimeBlocks(businessHours);
+
   $(".container").append(timeBlocks);
+  const callback = function () {
+    console.log(this);
+    console.log($(".time-item"));
+    if ($(this).is(".time-item")) {
+      const timeBlockHour = $(".time-item").text();
+      console.log(timeBlockHour);
+    }
+    // if (element === moment().format("HH")) {
+    // } else if (element > moment().format("HH")) {
+    // }
+  };
+  $(".row").each(callback);
+};
+
+//function to add text to text area
+const addActivityText = () => {
+  const activityText = $(".textarea").text();
+  return activityText;
 };
 
 //function to add activities to local storage
-// const addActivityToLocalStorage = () => {
-//   console.log("save");
-// };
+const addActivityToLocalStorage = () => {};
 
-//add event listener on save item
-// $(".save-item").on(
-//   "click",
-//   (addActivityToLocalStorage = () => {
-//     console.log("save");
-//   })
-// );
+//function to get from local storage
 
 $(document).ready(() => {
   renderTimeBlocks();
-  $(".save-item").on(
-    "click",
-    (addActivityToLocalStorage = () => {
-      console.log("save");
-    })
-  );
+  //add event listener on save item
+  $(".save-item").on("click", addActivityToLocalStorage());
 });
-//on window load
-// window.onload = () => {
-//   renderTimeBlocks();
-// };
