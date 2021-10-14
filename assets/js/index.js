95c4b666228c396639f46dcf2a0362156d78014e
liana valeria <liana.valeria15@gmail.com>
2021-10-14 12:38:30 +0100

    updated business hours array object

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,13 +8,23 @@ const today = moment().format("Do MMMM YYYY");
 dateContainer.text(today);
 
 //get business hours and store them into an array
-const businessHours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+const businessHours = [
+  { id: 9, hour: "9 AM", activityText: "" },
+  { id: 10, hour: "10 AM", activityText: "" },
+  { id: 11, hour: "11 AM", activityText: "" },
+  { id: 12, hour: "12 AM", activityText: "" },
+  { id: 13, hour: "1 PM", activityText: "" },
+  { id: 14, hour: "2 PM", activityText: "" },
+  { id: 15, hour: "3 AM", activityText: "" },
+  { id: 16, hour: "4 AM", activityText: "" },
+  { id: 17, hour: "5 AM", activityText: "" },
+];
 
 //build time blocks
 const constructTimeBlocks = (businessHours) => {
   const timeBlock = (element) => {
     const timeBlockDiv = `<div class="row">
-    <div class="col time-item">${element}</div>
+    <div class="col time-item">${element.hour}</div>
     <div class="col-8 past"><textarea class="textarea"></textarea></div>
     <div  class="col save-item"><i class="fas fa-save"></i></div>
   </div>`;
